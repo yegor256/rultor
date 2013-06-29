fc844afa1e3eeab0ddaf19a9dceb7d74c9726a77
Yegor Bugayenko <yegor@tpc2.com>
2013-06-29 18:16:31 +0200

    #40 implemented and tested

---
+++ rultor-drain/src/main/java/com/rultor/drain/Syslog.java
@@ -0,0 +1,148 @@
+/**
+ * Copyright (c) 2009-2013, rultor.com
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met: 1) Redistributions of source code must retain the above
+ * copyright notice, this list of conditions and the following
+ * disclaimer. 2) Redistributions in binary form must reproduce the above
+ * copyright notice, this list of conditions and the following
+ * disclaimer in the documentation and/or other materials provided
+ * with the distribution. 3) Neither the name of the rultor.com nor
+ * the names of its contributors may be used to endorse or promote
+ * products derived from this software without specific prior written
+ * permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT
+ * NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
+ * THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.rultor.drain;
+
+import com.jcabi.aspects.Immutable;
+import com.jcabi.aspects.Loggable;
+import com.rultor.spi.Drain;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.util.Date;
+import java.util.SortedSet;
+import java.util.TreeSet;
+import javax.validation.constraints.NotNull;
+import lombok.EqualsAndHashCode;
+import org.apache.commons.codec.CharEncoding;
+
+/**
+ * Syslog.
+ *
+ * @author Yegor Bugayenko (yegor@tpc2.com)
+ * @version $Id$
+ * @since 1.0
+ * @checkstyle ClassDataAbstractionCoupling (500 lines)
+ */
+@Immutable
+@EqualsAndHashCode
+@Loggable(Loggable.DEBUG)
+public final class Syslog implements Drain {
+
+    /**
+     * Host name.
+     */
+    private final transient String host;
+
+    /**
+     * UDP port number.
+     */
+    private final transient int port;
+
+    /**
+     * Facility.
+     */
+    private final transient String facility;
+
+    /**
+     * Public ctor.
+     * @param hst Host
+     * @param prt Port
+     * @param fac Facility
+     */
+    public Syslog(@NotNull final String hst, @NotNull final int prt,
+        @NotNull final String fac) {
+        this.host = hst;
+        this.port = prt;
+        this.facility = fac;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String toString() {
+        return String.format(
+            "`syslog://%s:%s` as `%s`",
+            this.host, this.port, this.facility
+        );
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public SortedSet<Long> pulses() throws IOException {
+        return new TreeSet<Long>();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void append(final long date, final Iterable<String> lines)
+        throws IOException {
+        final Date time = new Date(date);
+        for (String line : lines) {
+            this.send(
+                String.format(
+                    "%tF %<tT %s", time, line
+                ).getBytes(CharEncoding.UTF_8)
+            );
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public InputStream read(final long date) throws IOException {
+        throw new IllegalArgumentException("there are no pulses");
+    }
+
+    /**
+     * Send one packet.
+     * @param bytes Bytes to send
+     * @throws IOException If some IO problem
+     */
+    private void send(final byte[] bytes) throws IOException {
+        final DatagramSocket socket = new DatagramSocket();
+        socket.send(
+            new DatagramPacket(
+                bytes,
+                bytes.length,
+                InetAddress.getByName(this.host),
+                this.port
+            )
+        );
+    }
+
+}
---
+++ rultor-drain/src/test/java/com/rultor/drain/SyslogITCase.java
@@ -0,0 +1,56 @@
+/**
+ * Copyright (c) 2009-2013, rultor.com
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met: 1) Redistributions of source code must retain the above
+ * copyright notice, this list of conditions and the following
+ * disclaimer. 2) Redistributions in binary form must reproduce the above
+ * copyright notice, this list of conditions and the following
+ * disclaimer in the documentation and/or other materials provided
+ * with the distribution. 3) Neither the name of the rultor.com nor
+ * the names of its contributors may be used to endorse or promote
+ * products derived from this software without specific prior written
+ * permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT
+ * NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
+ * THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.rultor.drain;
+
+import com.jcabi.aspects.Tv;
+import com.rultor.spi.Drain;
+import java.util.Arrays;
+import org.junit.Test;
+
+/**
+ * Integration case for {@link Syslog}.
+ * @author Yegor Bugayenko (yegor@tpc2.com)
+ * @version $Id$
+ */
+public final class SyslogITCase {
+
+    /**
+     * Syslog can log.
+     * @throws Exception If some problem inside
+     */
+    @Test
+    public void logsMessages() throws Exception {
+        final String msg = "some test log message \u20ac";
+        final long date = Tv.MILLION;
+        final Drain drain = new Syslog("logs.papertrailapp.com", 33152, "TEST");
+        drain.append(date, Arrays.asList(msg));
+    }
+
+}
