b5abc745fab48205b6153e4abad72d3901b26555
Yegor Bugayenko <yegor@tpc2.com>
2013-07-06 17:33:20 +0200

    #46 more logs

--- rultor-conveyer/src/main/resources/start.sh
+++ rultor-conveyer/src/main/resources/start.sh
@@ -10,10 +10,13 @@ INSTANCE=`curl --silent http://169.254.169.254/latest/meta-data/instance-id`
 curl --silent https://raw.github.com/yegor256/rultor/master/rultor-conveyer/src/main/resources/ec2-pom.xml > pom.xml
 
 mvn test --quiet --update-snapshots \
-  "-Daws-key=${AWS_KEY}" "-Daws-secret=${AWS_SECRET}" \
-  "-Dsqs-url=${SQS_URL}" "-Ddynamo-prefix=${DYNAMO_PREFIX}"
+    "-Daws-key=${AWS_KEY}" "-Daws-secret=${AWS_SECRET}" \
+    "-Dsqs-url=${SQS_URL}" "-Ddynamo-prefix=${DYNAMO_PREFIX}"
 
 if [ $? -eq 0 ]; then
-  ec2-terminate-instances --aws-access-key "${AWS_KEY}" \
-    --aws-secret-key "${AWS_SECRET}" "${INSTANCE}"
+    echo "EC2 instance is going to terminate itself..."
+    ec2-terminate-instances --aws-access-key "${AWS_KEY}" \
+        --aws-secret-key "${AWS_SECRET}" "${INSTANCE}"
 fi
+
+echo "Something went wrong in Maven, see logs above"
