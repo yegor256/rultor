4058267737d807644268f35eea81a93ec9c3cf97
Yegor Bugayenko <yegor@tpc2.com>
2013-07-06 17:13:35 +0200

    #46 save to file first

--- rultor-conveyer/src/main/resources/start.sh
+++ rultor-conveyer/src/main/resources/start.sh
@@ -2,7 +2,9 @@ export M2_HOME="/usr/local/share/apache-maven"
 export PATH="${M2_HOME}/bin:${PATH}"
 export MAVEN_OPTS="-Xmx1024m -XX:MaxPermSize=256m"
 
-source `curl --silent http://169.254.169.254/latest/user-data`
+curl --silent http://169.254.169.254/latest/user-data > /tmp/variables.sh
+source /tmp/variables.sh
+
 INSTANCE=`curl --silent http://169.254.169.254/latest/meta-data/instance-id`
 
 curl --silent https://raw.github.com/yegor256/rultor/master/rultor-conveyer/src/main/resources/ec2-pom.xml > pom.xml
