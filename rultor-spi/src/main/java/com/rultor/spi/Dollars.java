76337c635e2edec8580b968fb85e099f286874b5
Yegor Bugayenko <yegor@tpc2.com>
2013-07-15 21:55:40 +0200

    #63 better formatting of dollars

--- rultor-spi/src/main/java/com/rultor/spi/Dollars.java
+++ rultor-spi/src/main/java/com/rultor/spi/Dollars.java
@@ -66,12 +66,12 @@ public final class Dollars {
         final double usd = Math.abs((double) this.amount / Tv.MILLION);
         String body;
         // @checkstyle MagicNumber (3 lines)
-        if (usd < 0.001d) {
-            body = String.format("$%.4f", usd);
-        } else if (usd < 0.01d) {
+        if (usd >= 0.01d || usd < 0.0001d) {
+            body = String.format("$%.2f", usd);
+        } else if (usd >= 0.001d) {
             body = String.format("$%.3f", usd);
         } else {
-            body = String.format("$%.2f", usd);
+            body = String.format("$%.4f", usd);
         }
         if (this.amount < 0) {
             body = String.format("(%s)", body);
