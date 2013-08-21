23434ca24c57cecf22aa88766cd2f446f60fec91
Yegor Bugayenko <yegor@tpc2.com>
2013-08-21 15:56:52 +0200

    #151 typo

--- rultor-conveyer/src/main/java/com/rultor/conveyer/audit/FreeTier.java
+++ rultor-conveyer/src/main/java/com/rultor/conveyer/audit/FreeTier.java
@@ -27,7 +27,7 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
  * OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-package com.rultor.users.audit;
+package com.rultor.conveyer.audit;
 
 import com.jcabi.aspects.Immutable;
 import com.jcabi.aspects.Loggable;
