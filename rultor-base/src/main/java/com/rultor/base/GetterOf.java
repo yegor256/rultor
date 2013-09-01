67db3c77fe8dd6bff8d474f6857e8779959a7e25
krzyk <Krzysztof.Krason@sabre.com>
2013-09-01 20:30:49 +0200

    #223 recognize short getter format

--- rultor-base/src/main/java/com/rultor/base/GetterOf.java
+++ rultor-base/src/main/java/com/rultor/base/GetterOf.java
@@ -32,10 +32,13 @@ package com.rultor.base;
 import com.jcabi.aspects.Immutable;
 import com.jcabi.aspects.Loggable;
 import com.rultor.spi.Proxy;
+import java.beans.BeanInfo;
 import java.beans.IntrospectionException;
 import java.beans.Introspector;
+import java.beans.MethodDescriptor;
 import java.beans.PropertyDescriptor;
 import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import javax.validation.constraints.NotNull;
 import lombok.EqualsAndHashCode;
 
@@ -75,24 +78,51 @@ public final class GetterOf implements Proxy<Object> {
     }
 
     /**
-     * {@inheritDoc}
+     * Find method with getter.
+     * @param info Bean info.
+     * @return Found method.
      */
-    @Override
-    public Object object() {
-        try {
-            for (PropertyDescriptor descr : Introspector
-                .getBeanInfo(this.source.getClass()).getPropertyDescriptors()) {
+    private Method find(final BeanInfo info) {
+        Method foundMethod = null;
+        boolean found = false;
+        for (PropertyDescriptor descr : info.getPropertyDescriptors()) {
+            if (descr.getName().equals(this.property)
+                && (descr.getReadMethod() != null)) {
+                foundMethod = descr.getReadMethod();
+                found = true;
+                break;
+            }
+        }
+        if (!found) {
+            for (MethodDescriptor descr : info.getMethodDescriptors()) {
                 if (descr.getName().equals(this.property)
-                    && (descr.getReadMethod() != null)) {
-                    return descr.getReadMethod().invoke(this.source);
+                    && (descr.getMethod().getParameterTypes().length == 0)) {
+                    foundMethod = descr.getMethod();
+                    found = true;
+                    break;
                 }
             }
+        }
+        if (found) {
+            return foundMethod;
+        } else {
             throw new IllegalArgumentException(
                 String.format(
                     "Object should have a getter for property '%s'",
                     this.property
                 )
             );
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Object object() {
+        try {
+            return this.find(Introspector.getBeanInfo(this.source.getClass()))
+                .invoke(this.source);
         } catch (IntrospectionException ex) {
             throw new IllegalArgumentException(ex);
         } catch (InvocationTargetException ex) {
