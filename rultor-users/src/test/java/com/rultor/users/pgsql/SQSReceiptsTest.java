0d152ce8569f1fb20de964864eb0661acc0e6c01
Yegor Bugayenko <yegor@tpc2.com>
2013-09-01 13:59:33 +0200

    #206 SQSReceiptsTest fixed

--- rultor-users/src/test/java/com/rultor/users/pgsql/SQSReceiptsTest.java
+++ rultor-users/src/test/java/com/rultor/users/pgsql/SQSReceiptsTest.java
@@ -64,7 +64,7 @@ public final class SQSReceiptsTest {
             new ReceiveMessageResult().withMessages(
                 new Message().withBody(
                     // @checkstyle StringLiteralsConcatenation (5 lines)
-                    "{\"work\": {\"owner\": \"urn:test:1\", \"unit\": \"test\","
+                    "{\"work\": {\"owner\": \"urn:test:1\", \"rule\": \"test\","
                     + " \"scheduled\": \"2012-08-23T13:25:33Z\"},"
                     + "\"ct\":\"urn:test:2\", \"ctrule\":\"some-other-2\","
                     + "\"dt\":\"urn:test:3\", \"dtrule\":\"some-other-4\","
@@ -110,7 +110,7 @@ public final class SQSReceiptsTest {
         Mockito.doReturn(
             new ReceiveMessageResult().withMessages(
                 new Message().withBody(
-                    "{\"work\": {\"unit\":\"x\",\"owner\":\"urn:test:1\"}}"
+                    "{\"work\": {\"rule\":\"x\",\"owner\":\"urn:test:1\"}}"
                 )
             )
         ).when(aws).receiveMessage(Mockito.any(ReceiveMessageRequest.class));
