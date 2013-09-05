6f25c4b624bb6fe14507c47352228385c1aa9655
anouarbadri <anouar_badri@yahoo.fr>
2013-09-05 13:13:23 +0200

    211
    fix checkstyle violations

--- rultor-base/src/test/java/com/rultor/shell/TerminalTest.java
+++ rultor-base/src/test/java/com/rultor/shell/TerminalTest.java
@@ -1,56 +1,69 @@
+/**
+ * Copyright (c) 2009-2013, rultor.com
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met: 1) Redistributions of source code must retain the above
+ * copyright notice, this list of conditions and the following
+ * disclaimer. 2) Redistributions in binary form must reproduce the above
+ * copyright notice, this list of conditions and the following
+ * disclaimer in the documentation and/or other materials provided
+ * with the distribution. 3) Neither the name of the rultor.com nor
+ * the names of its contributors may be used to endorse or promote
+ * products derived from this software without specific prior written
+ * permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT
+ * NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
+ * THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 package com.rultor.shell;
 
-import static org.junit.Assert.*;
 import java.io.IOException;
 import java.io.InputStream;
 import org.apache.commons.io.output.TeeOutputStream;
+import org.junit.Assert;
 import org.junit.Test;
 import org.mockito.Mockito;
 
 /**
  * Test case for {@link Terminal}.
- * 
- * @author anouar.badri
+ * @author Anouar Badri (anouar_badri@yahoo.fr)
  * @version $Id$
  */
 public final class TerminalTest {
 
-	/**
-	 * terminal.exec must throw an instance of IOException when the method
-	 * shell.exec() terminate with non-zero exit code.
-	 * 
-	 * @throws Exception
-	 *             If some problem inside
-	 */
-	@Test
-	@SuppressWarnings("unchecked")
-	public void returnsNonZeroreturnsCode() throws Exception {
-		final Shell shell = Mockito.mock(Shell.class);
-
-		Mockito.doReturn(1)
-				.when(shell)
-				.exec(Mockito.anyString(), Mockito.any(InputStream.class),
-						Mockito.any(TeeOutputStream.class),
-						Mockito.any(TeeOutputStream.class));
-
-		final Terminal terminal = new Terminal(shell);
-
-		try {
-			terminal.exec("", "");
-			fail("should throw IOException"); // fail if no exception is thrown
-
-		}
-		// must throw IOException
-		catch (IOException ex) {
-             assertTrue(true);
-		}
-		// should not throw any other exception
-		catch (Exception ex) {
-			fail("other exception was thrown"); // fail if no exception is
-												// thrown
-
-		}
-
-	}
+    /**
+     * Terminal.exec must throw an instance of IOException when the method
+     * shell.exec() terminate with non-zero exit code.
+     * @throws Exception If some problem inside
+     */
+    @Test
+    @SuppressWarnings("unchecked")
+    public void returnsNonZeroreturnsCode() throws Exception {
+        final Shell shell = Mockito.mock(Shell.class);
+        Mockito.doReturn(1).when(shell).exec(
+            Mockito.anyString(),
+            Mockito.any(InputStream.class),
+            Mockito.any(TeeOutputStream.class),
+            Mockito.any(TeeOutputStream.class)
+        );
+        final Terminal terminal = new Terminal(shell);
+        try {
+            terminal.exec("", "");
+            Assert.fail("should throw IOException");
+        } catch (IOException ex) {
+            Assert.assertTrue(true);
+        }
+    }
 }
-
