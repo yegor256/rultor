8e4cb818d04f695c35b15983d5ed1fef1b14fa73
Yegor Bugayenko <yegor@tpc2.com>
2013-09-09 08:54:23 +0200

    Crontab step reporting fixed

--- rultor-base/src/main/java/com/rultor/base/Crontab.java
+++ rultor-base/src/main/java/com/rultor/base/Crontab.java
@@ -166,7 +166,7 @@ public final class Crontab implements Instance {
      * Execution allowed?
      * @return TRUE if allowed
      */
-    @Step("Crontab `${this.rules()}` #if(!$result)NOT#end allowed execution")
+    @Step("Crontab `${this.rules}` #if(!$result)NOT#end allowed execution")
     private boolean allowed() {
         final Calendar today = Crontab.calendar(this.work.scheduled());
         Crontab.Gate<Calendar> denier = null;
