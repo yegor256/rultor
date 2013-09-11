57a4315f8d64e1ee100c5ad95730798b1031d72b
Yegor Bugayenko <yegor@tpc2.com>
2013-09-11 22:10:43 +0200

    #264 added missed integration with Github

--- rultor-base/src/main/java/com/rultor/guard/github/AssignedTo.java
+++ rultor-base/src/main/java/com/rultor/guard/github/AssignedTo.java
@@ -64,6 +64,9 @@ public final class AssignedTo implements Approval {
         this.user = assignee;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public boolean has(final PullRequest request, final Github client,
         final Github.Repo repo) throws IOException {
