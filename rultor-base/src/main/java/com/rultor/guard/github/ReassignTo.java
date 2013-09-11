4230150280e885f168e1e6bb1dc4f9602881c879
Yegor Bugayenko <yegor@tpc2.com>
2013-09-11 19:56:34 +0200

    #280 typos

--- rultor-base/src/main/java/com/rultor/guard/github/ReassignTo.java
+++ rultor-base/src/main/java/com/rultor/guard/github/ReassignTo.java
@@ -48,7 +48,7 @@ import org.eclipse.egit.github.core.service.UserService;
 @ToString
 @EqualsAndHashCode(of = { "user" })
 @Loggable(Loggable.DEBUG)
-final class ReassignTo implements Approval {
+public final class ReassignTo implements Approval {
 
     /**
      * Reassigned to user.
@@ -59,7 +59,7 @@ final class ReassignTo implements Approval {
      * Public ctor.
      * @param assignee Assignee
      */
-    protected ReassignTo(
+    public ReassignTo(
         @NotNull(message = "asignee can not be null") final String assignee) {
         this.user = assignee;
     }
