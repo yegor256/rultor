1a871e9c84547087a3cda1dc22b633f6d419715b
Yegor Bugayenko <yegor@tpc2.com>
2013-09-14 17:29:25 +0200

    #92 integration tests for MongoStands and MongoUsers

---
+++ rultor-users/src/test/java/com/rultor/users/mongo/MongoStandsITCase.java
@@ -0,0 +1,122 @@
+/**
+ * Copyright (c) 2009-2013, rultor.com
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met: 1) Redistributions of source code must retain the above
+ * copyright notice, this list of conditions and the following
+ * disclaimer. 2) Redistributions in binary form must reproduce the above
+ * copyright notice, this list of conditions and the following
+ * disclaimer in the documentation and/or other materials provided
+ * with the distribution. 3) Neither the name of the rultor.com nor
+ * the names of its contributors may be used to endorse or promote
+ * products derived from this software without specific prior written
+ * permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT
+ * NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
+ * THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.rultor.users.mongo;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBCollection;
+import com.rultor.spi.Pulse;
+import com.rultor.spi.Stand;
+import com.rultor.spi.Stands;
+import java.util.Arrays;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.junit.Assume;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+/**
+ * Integration case for {@link MongoStands}.
+ * @author Yegor Bugayenko (yegor@tpc2.com)
+ * @version $Id$
+ * @checkstyle ClassDataAbstractionCoupling (500 lines)
+ */
+public final class MongoStandsITCase {
+
+    /**
+     * Mongo host.
+     */
+    private static final String HOST =
+        System.getProperty("failsafe.mongo.host");
+
+    /**
+     * Mongo port.
+     */
+    private static final String PORT =
+        System.getProperty("failsafe.mongo.port");
+
+    /**
+     * Mongo database name.
+     */
+    private static final String NAME =
+        System.getProperty("failsafe.mongo.name");
+
+    /**
+     * Mongo user.
+     */
+    private static final String USER =
+        System.getProperty("failsafe.mongo.user");
+
+    /**
+     * Mongo password.
+     */
+    private static final String PWD =
+        System.getProperty("failsafe.mongo.password");
+
+    /**
+     * MongoStands can show full flow.
+     * @throws Exception If some problem inside
+     */
+    @Test
+    public void showsFlowOfPulsesFromMongo() throws Exception {
+        final Mongo mongo = this.mongo();
+        final DBCollection col = mongo.get().getCollection(MongoStand.TABLE);
+        col.remove(new BasicDBObject());
+        final String name = "my-stand";
+        final Stand stand = Mockito.mock(Stand.class);
+        Mockito.doReturn(name).when(stand).name();
+        col.insert(new BasicDBObject().append(MongoStand.ATTR_STAND, name));
+        col.insert(new BasicDBObject().append(MongoStand.ATTR_STAND, "x"));
+        col.insert(new BasicDBObject().append(MongoStand.ATTR_STAND, "y"));
+        final Stands stands = Mockito.mock(Stands.class);
+        Mockito.doReturn(Arrays.asList(stand).iterator())
+            .when(stands).iterator();
+        MatcherAssert.assertThat(
+            new MongoStands(mongo, stands).flow(),
+            Matchers.<Pulse>iterableWithSize(1)
+        );
+    }
+
+    /**
+     * Get Mongo client to test against.
+     * @return Client
+     * @throws Exception If some problem inside
+     */
+    private Mongo mongo() throws Exception {
+        Assume.assumeNotNull(MongoStandsITCase.HOST);
+        return new Mongo.Simple(
+            MongoStandsITCase.HOST,
+            Integer.parseInt(MongoStandsITCase.PORT),
+            MongoStandsITCase.NAME,
+            MongoStandsITCase.USER,
+            MongoStandsITCase.PWD
+        );
+    }
+
+}
