e29f549b8b4b37e08584edb3b62aaac616a49af4
shailendrasoni <soni_shailenda0@yahoo.com>
2013-10-09 02:13:08 -0700

    #238 Changes based on Qulice Rules

--- rultor-web/src/test/java/com/rultor/web/AuthKeysTest.java
+++ rultor-web/src/test/java/com/rultor/web/AuthKeysTest.java
@@ -1,7 +1,7 @@
 /**
  * Copyright (c) 2009-2013, rultor.com
  * All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
  * are met: 1) Redistributions of source code must retain the above
@@ -13,7 +13,7 @@
  * the names of its contributors may be used to endorse or promote
  * products derived from this software without specific prior written
  * permission.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
  * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT
  * NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
@@ -29,85 +29,71 @@
  */
 package com.rultor.web;
 
-import java.io.IOException;
+import com.rexsl.page.auth.Identity;
 import javax.validation.ConstraintViolationException;
 import org.hamcrest.MatcherAssert;
 import org.hamcrest.Matchers;
-import org.junit.Before;
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.ExpectedException;
-import com.jcabi.manifests.Manifests;
-import com.rexsl.page.auth.Identity;
 
 /**
  * Test case for {@link AuthKeys}.
- * @author Shailendra Soni
+ * @author Shailendra Soni (soni_shailendra02@yahoo.com)
  * @version $Id$
  */
-
 public class AuthKeysTest {
 
-	/**
-	 * Pre-load test MANIFEST.MF.
-	 * @throws IOExceptionIf fails
-	 */
-	@Rule
-	public final ExpectedException thrown = ExpectedException.none();
+    /**
+     * Validate weather user is null or not.
+     */
+    @Test(expected = ConstraintViolationException.class)
+    public final void validateNullUser() {
+        final AuthKeys authKeys = new AuthKeys();
+        final String user = null;
+        final String password = "password";
+        authKeys.authenticate(user, password);
+    }
 
-	@Before
-	public void manifests() throws IOException {
-		Manifests.inject("Rultor-Revision", "12345");
-	}
+    /**
+     * It should be throw exception if password is given null.
+     */
+    @Test(expected = ConstraintViolationException.class)
+    public final void validateNullPassword() {
+        final AuthKeys authKeys = new AuthKeys();
+        final String user = "urn:git:soni";
+        final String password = null;
+        authKeys.authenticate(user, password);
+    }
 
-	/**
-	 * Validate weather user is null or not.
-	 */
-	@Test
-	public void validateNullUser() {
-		thrown.expect(ConstraintViolationException.class);
-		final AuthKeys authKeys = new AuthKeys();
-		final String user = null;
-		final String password = "test";
-		authKeys.authenticate(user, password);
-	}
+    /**
+     * Giving proper data to authenticate.
+     */
+    @Test
+    public final void authenticateURN() {
+        final AuthKeys authKeys = new AuthKeys();
+        final String user = "urn:git:rultor";
+        final String password = "test";
+        MatcherAssert.assertThat(
+            authKeys.authenticate(user, password)
+                .name(),
+                Matchers.equalTo(Identity.ANONYMOUS.name())
+        );
+    }
 
-	/**
-	 * It should be throw exception if password is given null.
-	 */
-	@Test
-	public void validateNullPassword() {
-		thrown.expect(ConstraintViolationException.class);
-		final AuthKeys authKeys = new AuthKeys();
-		final String user = "urn:git:test";
-		final String password = null;
-		authKeys.authenticate(user, password);
-	}
-
-	/**
-	 * Giving proper data to authenticate.
-	 */
-	@Test
-	public void authenticateURN() {
-		final AuthKeys authKeys = new AuthKeys();
-		final String user = "urn:git:test";
-		final String password = "test";
-		MatcherAssert.assertThat(authKeys.authenticate(user, password).name(),
-		        Matchers.equalTo(Identity.ANONYMOUS.name()));
-	}
-
-	
-	/**
-	 * @TODO #123? , Ideally target class AuthKeys should also have validation of empty string.
-	 */
-	@Test
-	public void authenticateURNWithEmptyUser() {
-		thrown.expect(IllegalArgumentException.class);
-		final AuthKeys authKeys = new AuthKeys();
-		final String user = "";
-		final String password = "";
-		MatcherAssert.assertThat(authKeys.authenticate(user, password).name(),
-		        Matchers.equalTo(Identity.ANONYMOUS.name()));
-	}
+    /**
+     * Execute method with Empty String.
+     * @TODO #123?,Ideally target class AuthKeys should also have
+     *  validation of empty string.
+     */
+    @Test(expected = IllegalArgumentException.class)
+    public final void authenticateURNWithEmptyUser() {
+        final AuthKeys authKeys = new AuthKeys();
+        final String user = "";
+        final String password = "";
+        MatcherAssert.assertThat(
+            authKeys.authenticate(user, password)
+                .name(),
+                Matchers.equalTo(Identity.ANONYMOUS.name())
+        );
+    }
 
 }
