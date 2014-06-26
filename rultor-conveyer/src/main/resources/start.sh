038e7be6ef993d86b72adb11188d33a15866b232
Yegor Bugayenko <yegor@tpc2.com>
2014-06-26 19:26:53 +0200

    year updated to 2014

--- rultor-conveyer/src/main/resources/start.sh
+++ rultor-conveyer/src/main/resources/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -x
 #
-# Copyright (c) 2009-2013, rultor.com
+# Copyright (c) 2009-2014, rultor.com
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
