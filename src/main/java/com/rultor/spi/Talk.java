d1d9467f34e40cdfb8f6d9d2021aa223a5a5f72d
Yegor Bugayenko <yegor@tpc2.com>
2014-07-07 23:57:26 +0200

    test

--- src/main/java/com/rultor/spi/Talk.java
+++ src/main/java/com/rultor/spi/Talk.java
@@ -31,8 +31,15 @@ package com.rultor.spi;
 
 import com.jcabi.aspects.Immutable;
 import com.jcabi.xml.XML;
+import com.jcabi.xml.XMLDocument;
+import java.io.File;
 import java.io.IOException;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.CharEncoding;
+import org.w3c.dom.Node;
 import org.xembly.Directive;
+import org.xembly.ImpossibleModificationException;
+import org.xembly.Xembler;
 
 /**
  * Talk.
@@ -71,5 +78,61 @@ public interface Talk {
      * @throws IOException If fails
      */
     void active(boolean yes) throws IOException;
+
+    /**
+     * In file.
+     */
+    @Immutable
+    final class InFile implements Talk {
+        /**
+         * File.
+         */
+        private final transient String path;
+        /**
+         * Ctor.
+         * @throws IOException If fails
+         */
+        public InFile() throws IOException {
+            this(File.createTempFile("rultor", ".talk"));
+            FileUtils.write(new File(this.path), "<talk/>");
+        }
+        /**
+         * Ctor.
+         * @param file The file
+         */
+        public InFile(final File file) {
+            this.path = file.getAbsolutePath();
+        }
+        @Override
+        public String name() {
+            return "a1b2c3d4";
+        }
+        @Override
+        public XML read() throws IOException {
+            return new XMLDocument(
+                FileUtils.readFileToString(
+                    new File(this.path), CharEncoding.UTF_8
+                )
+            );
+        }
+        @Override
+        public void modify(final Iterable<Directive> dirs) throws IOException {
+            final Node node = this.read().node();
+            try {
+                new Xembler(dirs).apply(node);
+            } catch (final ImpossibleModificationException ex) {
+                throw new IllegalStateException(ex);
+            }
+            FileUtils.write(
+                new File(this.path),
+                new XMLDocument(node).toString(),
+                CharEncoding.UTF_8
+            );
+        }
+        @Override
+        public void active(final boolean yes) {
+            // nothing
+        }
+    }
 
 }
