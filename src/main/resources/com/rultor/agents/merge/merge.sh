0be083f931e5d017d4250e27feba09f5e645e4b6
Yegor Bugayenko <yegor@tpc2.com>
2014-07-07 18:47:30 +0200

    better checking for merge script

--- src/main/resources/com/rultor/agents/merge/merge.sh
+++ src/main/resources/com/rultor/agents/merge/merge.sh
@@ -5,14 +5,20 @@ git remote add head "${HEAD}"
 git remote update
 git merge "head/${HEAD-BRANCH}"
 
-if [ -e pom.xml ]; then
-  CMD="mvn help:system clean install --batch-mode --update-snapshots --errors --strict-checksums"
-fi
-if [ -e build.xml ]; then
-  CMD="ant"
-fi
-if [ -e Gemfile ]; then
-  CMD="bundle test"
+if [ -z "${SCRIPT}" ]; then
+  if [ -e pom.xml ]; then
+    SCRIPT="mvn help:system clean install --batch-mode --update-snapshots --errors --strict-checksums"
+  fi
+  if [ -e build.xml ]; then
+    SCRIPT="ant"
+  fi
+  if [ -e Gemfile ]; then
+    SCRIPT="bundle test"
+  fi
+  if [ -z "${SCRIPT}" ]; then
+    echo "merge.script is not defined in .rultor.yml and project root directory doesn't explain what this project is built with"
+    exit -1
+  fi
 fi
 
 sudo docker run --rm -v $(pwd):/main -w=/main yegor256/rultor "${SCRIPT}"
