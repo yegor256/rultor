75419b120597d6226fd09ffd139326f25d505027
Yegor Bugayenko <yegor@tpc2.com>
2014-07-09 08:13:38 +0200

    location of assets

--- src/main/resources/com/rultor/agents/merge/merge.sh
+++ src/main/resources/com/rultor/agents/merge/merge.sh
@@ -6,6 +6,7 @@ git remote update
 git config user.email "me@rultor.com"
 git config user.name "rultor"
 git merge "head/${HEAD_BRANCH}"
+cd ..
 
 if [ -z "${SCRIPT}" ]; then
   if [ -e pom.xml ]; then
@@ -23,11 +24,13 @@ if [ -z "${SCRIPT}" ]; then
   fi
 fi
 
-BIN=.rultor.sh
+BIN=merge.sh
 echo "#!/bin/bash" > ${BIN}
+echo "cd repo" >> ${BIN}
 echo "${SCRIPT[@]}" >> ${BIN}
 chmod a+x ${BIN}
 
 sudo docker run --rm -v $(pwd):/main "${DOCKER_ENVS[@]}" -w=/main yegor256/rultor /main/${BIN}
 
+cd repo
 git push origin "${BASE_BRANCH}"
