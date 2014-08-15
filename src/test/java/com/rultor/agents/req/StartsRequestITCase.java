4bf0ec32d2088cba9ef6efab0dffedb970c04bfa
Yegor Bugayenko <yegor@tpc2.com>
2014-08-15 16:38:51 +0200

    #548 test for live docker

--- src/test/java/com/rultor/agents/req/StartsRequestITCase.java
+++ src/test/java/com/rultor/agents/req/StartsRequestITCase.java
@@ -30,21 +30,14 @@
 package com.rultor.agents.req;
 
 import com.google.common.base.Joiner;
-import com.jcabi.immutable.Array;
-import com.jcabi.log.VerboseProcess;
 import com.jcabi.xml.XMLDocument;
+import com.rultor.agents.shells.SSH;
+import com.rultor.agents.shells.Shell;
 import com.rultor.spi.Agent;
 import com.rultor.spi.Profile;
 import com.rultor.spi.Talk;
-import java.io.File;
-import java.io.IOException;
-import org.apache.commons.lang3.StringUtils;
-import org.hamcrest.Matcher;
-import org.hamcrest.MatcherAssert;
-import org.hamcrest.Matchers;
-import org.junit.Rule;
+import org.junit.Assume;
 import org.junit.Test;
-import org.junit.rules.TemporaryFolder;
 import org.xembly.Directives;
 
 /**
@@ -52,34 +45,67 @@ import org.xembly.Directives;
  *
  * @author Yegor Bugayenko (yegor@tpc2.com)
  * @version $Id$
- * @since 1.4
+ * @since 1.24.1
  * @checkstyle ClassDataAbstractionCouplingCheck (500 lines)
  * @checkstyle MultipleStringLiteralsCheck (500 lines)
  */
-@SuppressWarnings("PMD.AvoidDuplicateLiterals")
 public final class StartsRequestITCase {
 
     /**
-     * Temp directory.
-     * @checkstyle VisibilityModifierCheck (5 lines)
+     * Docker server address.
      */
-    @Rule
-    public final transient TemporaryFolder temp = new TemporaryFolder();
+    private static final String HOST =
+        System.getProperty("failsafe.docker.host");
 
     /**
-     * StartsRequest can start a request.
+     * Docker server username.
+     */
+    private static final String LOGIN =
+        System.getProperty("failsafe.docker.login");
+
+    /**
+     * Docker server SSH key.
+     */
+    private static final String KEY =
+        System.getProperty("failsafe.docker.key");
+
+    /**
+     * StartsRequest can compose an executable DEPLOY request.
      * @throws Exception In case of error.
      */
     @Test
-    public void startsDeployRequest() throws Exception {
-        final File repo = this.repo();
+    public void composesCorrectDeployRequest() throws Exception {
+        Assume.assumeNotNull(StartsRequestITCase.HOST);
+        final Shell shell = new Shell.Safe(
+            new SSH(
+                StartsRequestITCase.HOST, 22,
+                StartsRequestITCase.LOGIN, StartsRequestITCase.KEY
+            )
+        );
+        new Shell.Plain(shell).exec(
+            Joiner.on(';').join(
+                "cd /tmp",
+                "git init repo.git",
+                "cd repo.git",
+                "git config user.email test@rultor.com",
+                "git config user.name test",
+                "echo 'hello, world!' > hello.txt",
+                "git add .",
+                "git commit -am 'first file'",
+                "git checkout -b frk",
+                "echo 'good bye!' > hello.txt",
+                "git commit -am 'modified file'",
+                "git checkout master",
+                "git config receive.denyCurrentBranch ignore"
+            )
+        );
         final Agent agent = new StartsRequest(
             new Profile.Fixed(
                 new XMLDocument(
-                    StringUtils.join(
-                        "<p><deploy><script>echo HEY</script>",
-                        "<env><MAVEN_OPTS>-Xmx2g -Xms1g</MAVEN_OPTS></env>",
-                        "</deploy></p>"
+                    Joiner.on(' ').join(
+                        "<p><deploy><script>",
+                        "id | grep rultor",
+                        "</script></deploy></p>"
                     )
                 )
             )
@@ -90,141 +116,13 @@ public final class StartsRequestITCase {
                 .add("request").attr("id", "abcd")
                 .add("type").set("deploy").up()
                 .add("args")
-                .add("arg").attr("name", "head").set(repo.toString()).up()
+                .add("arg").attr("name", "head").set("/tmp/repo.git").up()
                 .add("arg").attr("name", "head_branch").set("master").up()
         );
         agent.execute(talk);
-        MatcherAssert.assertThat(
-            this.exec(talk),
-            Matchers.allOf(
-                new Array<Matcher<? super String>>()
-                    .with(Matchers.containsString("image=yegor256/rultor\n"))
-                    .with(Matchers.containsString("Cloning into 'repo'...\n"))
-                    .with(Matchers.containsString("docker_when_possible\n"))
-                    .with(Matchers.containsString("image=yegor256/rultor"))
-                    .with(Matchers.containsString("load average is "))
-                    .with(Matchers.containsString("low enough to run a"))
-                    .with(
-                        Matchers.containsString(
-                            "DOCKER-5: --env=MAVEN_OPTS=-Xmx2g -Xms1g"
-                        )
-                    )
-            )
-        );
-    }
-
-    /**
-     * StartsRequest can start a release request.
-     * @throws Exception In case of error.
-     */
-    @Test
-    public void startsReleaseRequest() throws Exception {
-        final File repo = this.repo();
-        final Agent agent = new StartsRequest(
-            new Profile.Fixed(
-                new XMLDocument(
-                    "<p><release><script>echo HEY</script></release></p>"
-                )
-            )
-        );
-        final Talk talk = new Talk.InFile();
-        talk.modify(
-            new Directives().xpath("/talk")
-                .add("request").attr("id", "a8b9c0")
-                .add("type").set("release").up()
-                .add("args")
-                .add("arg").attr("name", "head").set(repo.toString()).up()
-                .add("arg").attr("name", "head_branch").set("master").up()
-                .add("arg").attr("name", "tag").set("1.0-beta").up()
-        );
-        agent.execute(talk);
-        this.exec(talk);
-    }
-
-    /**
-     * StartsRequest can start a merge request.
-     * @throws Exception In case of error.
-     */
-    @Test
-    public void startsMergeRequest() throws Exception {
-        final File repo = this.repo();
-        final Agent agent = new StartsRequest(
-            new Profile.Fixed(
-                new XMLDocument(
-                    "<p><merge><script>echo HEY</script></merge></p>"
-                )
-            )
-        );
-        final Talk talk = new Talk.InFile();
-        talk.modify(
-            new Directives().xpath("/talk")
-                .add("request").attr("id", "a1b2c3")
-                .add("type").set("merge").up()
-                .add("args")
-                .add("arg").attr("name", "head").set(repo.toString()).up()
-                .add("arg").attr("name", "head_branch").set("master").up()
-                .add("arg").attr("name", "fork").set(repo.toString()).up()
-                .add("arg").attr("name", "fork_branch").set("frk").up()
-        );
-        agent.execute(talk);
-        this.exec(talk);
-    }
-
-    /**
-     * Execute script from daemon.
-     * @param talk Talk to use
-     * @return Full stdout
-     * @throws IOException If fails
-     */
-    private String exec(final Talk talk) throws IOException {
-        final String script = Joiner.on('\n').join(
-            "set -x",
-            "set -e",
-            "set -o pipefail",
-            "function docker {",
-            "  for (( i=1; i<=$#; i++ )); do",
-            "    echo \"DOCKER-$i: ${!i}\"",
-            "  done",
-            "}",
+        new Shell.Plain(shell).exec(
             talk.read().xpath("//script/text()").get(0)
         );
-        return new VerboseProcess(
-            new ProcessBuilder().command(
-                "/bin/bash", "-c", script
-            ).directory(this.temp.newFolder()).redirectErrorStream(true)
-        ).stdout();
-    }
-
-    /**
-     * Create empty Git repo.
-     * @return Its location
-     * @throws IOException If fails
-     */
-    private File repo() throws IOException {
-        final File repo = this.temp.newFolder();
-        new VerboseProcess(
-            new ProcessBuilder().command(
-                "/bin/bash",
-                "-c",
-                Joiner.on(';').join(
-                    "set -x",
-                    "set -e",
-                    "set -o pipefail",
-                    "git init .",
-                    "git config user.email test@rultor.com",
-                    "git config user.name test",
-                    "echo 'hello, world!' > hello.txt",
-                    "git add .",
-                    "git commit -am 'first file'",
-                    "git checkout -b frk",
-                    "echo 'good bye!' > hello.txt",
-                    "git commit -am 'modified file'",
-                    "git checkout master",
-                    "git config receive.denyCurrentBranch ignore"
-                )
-            ).directory(repo)
-        ).stdout();
-        return repo;
     }
 
 }
--- src/test/java/com/rultor/agents/req/StartsRequestTest.java
+++ src/test/java/com/rultor/agents/req/StartsRequestTest.java
@@ -29,12 +29,23 @@
  */
 package com.rultor.agents.req;
 
+import com.google.common.base.Joiner;
+import com.jcabi.immutable.Array;
+import com.jcabi.log.VerboseProcess;
 import com.jcabi.matchers.XhtmlMatchers;
+import com.jcabi.xml.XMLDocument;
 import com.rultor.spi.Agent;
 import com.rultor.spi.Profile;
 import com.rultor.spi.Talk;
+import java.io.File;
+import java.io.IOException;
+import org.apache.commons.lang3.StringUtils;
+import org.hamcrest.Matcher;
 import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 import org.xembly.Directives;
 
 /**
@@ -43,8 +54,17 @@ import org.xembly.Directives;
  * @author Yegor Bugayenko (yegor@tpc2.com)
  * @version $Id$
  * @since 1.3
+ * @checkstyle ClassDataAbstractionCouplingCheck (500 lines)
+ * @checkstyle MultipleStringLiteralsCheck (500 lines)
  */
 public final class StartsRequestTest {
+
+    /**
+     * Temp directory.
+     * @checkstyle VisibilityModifierCheck (5 lines)
+     */
+    @Rule
+    public final transient TemporaryFolder temp = new TemporaryFolder();
 
     /**
      * StartsRequest can start a request.
@@ -70,6 +90,167 @@ public final class StartsRequestTest {
                 "//script[contains(.,'hey=hello!')]"
             )
         );
+    }
+
+    /**
+     * StartsRequest can start a request.
+     * @throws Exception In case of error.
+     */
+    @Test
+    public void startsDeployRequest() throws Exception {
+        final File repo = this.repo();
+        final Agent agent = new StartsRequest(
+            new Profile.Fixed(
+                new XMLDocument(
+                    StringUtils.join(
+                        "<p><deploy><script>echo HEY</script>",
+                        "<env><MAVEN_OPTS>-Xmx2g -Xms1g</MAVEN_OPTS></env>",
+                        "</deploy></p>"
+                    )
+                )
+            )
+        );
+        final Talk talk = new Talk.InFile();
+        talk.modify(
+            new Directives().xpath("/talk")
+                .add("request").attr("id", "abcd")
+                .add("type").set("deploy").up()
+                .add("args")
+                .add("arg").attr("name", "head").set(repo.toString()).up()
+                .add("arg").attr("name", "head_branch").set("master").up()
+        );
+        agent.execute(talk);
+        MatcherAssert.assertThat(
+            this.exec(talk),
+            Matchers.allOf(
+                new Array<Matcher<? super String>>()
+                    .with(Matchers.containsString("image=yegor256/rultor\n"))
+                    .with(Matchers.containsString("Cloning into 'repo'...\n"))
+                    .with(Matchers.containsString("docker_when_possible\n"))
+                    .with(Matchers.containsString("image=yegor256/rultor"))
+                    .with(Matchers.containsString("load average is "))
+                    .with(Matchers.containsString("low enough to run a"))
+                    .with(
+                        Matchers.containsString(
+                            "DOCKER-5: --env=MAVEN_OPTS=-Xmx2g -Xms1g"
+                        )
+                    )
+            )
+        );
+    }
+
+    /**
+     * StartsRequest can start a release request.
+     * @throws Exception In case of error.
+     */
+    @Test
+    public void startsReleaseRequest() throws Exception {
+        final File repo = this.repo();
+        final Agent agent = new StartsRequest(
+            new Profile.Fixed(
+                new XMLDocument(
+                    "<p><release><script>echo HEY</script></release></p>"
+                )
+            )
+        );
+        final Talk talk = new Talk.InFile();
+        talk.modify(
+            new Directives().xpath("/talk")
+                .add("request").attr("id", "a8b9c0")
+                .add("type").set("release").up()
+                .add("args")
+                .add("arg").attr("name", "head").set(repo.toString()).up()
+                .add("arg").attr("name", "head_branch").set("master").up()
+                .add("arg").attr("name", "tag").set("1.0-beta").up()
+        );
+        agent.execute(talk);
+        this.exec(talk);
+    }
+
+    /**
+     * StartsRequest can start a merge request.
+     * @throws Exception In case of error.
+     */
+    @Test
+    public void startsMergeRequest() throws Exception {
+        final File repo = this.repo();
+        final Agent agent = new StartsRequest(
+            new Profile.Fixed(
+                new XMLDocument(
+                    "<p><merge><script>echo HEY</script></merge></p>"
+                )
+            )
+        );
+        final Talk talk = new Talk.InFile();
+        talk.modify(
+            new Directives().xpath("/talk")
+                .add("request").attr("id", "a1b2c3")
+                .add("type").set("merge").up()
+                .add("args")
+                .add("arg").attr("name", "head").set(repo.toString()).up()
+                .add("arg").attr("name", "head_branch").set("master").up()
+                .add("arg").attr("name", "fork").set(repo.toString()).up()
+                .add("arg").attr("name", "fork_branch").set("frk").up()
+        );
+        agent.execute(talk);
+        this.exec(talk);
+    }
+
+    /**
+     * Execute script from daemon.
+     * @param talk Talk to use
+     * @return Full stdout
+     * @throws java.io.IOException If fails
+     */
+    private String exec(final Talk talk) throws IOException {
+        final String script = Joiner.on('\n').join(
+            "set -x",
+            "set -e",
+            "set -o pipefail",
+            "function docker {",
+            "  for (( i=1; i<=$#; i++ )); do",
+            "    echo \"DOCKER-$i: ${!i}\"",
+            "  done",
+            "}",
+            talk.read().xpath("//script/text()").get(0)
+        );
+        return new VerboseProcess(
+            new ProcessBuilder().command(
+                "/bin/bash", "-c", script
+            ).directory(this.temp.newFolder()).redirectErrorStream(true)
+        ).stdout();
+    }
+
+    /**
+     * Create empty Git repo.
+     * @return Its location
+     * @throws IOException If fails
+     */
+    private File repo() throws IOException {
+        final File repo = this.temp.newFolder();
+        new VerboseProcess(
+            new ProcessBuilder().command(
+                "/bin/bash",
+                "-c",
+                Joiner.on(';').join(
+                    "set -x",
+                    "set -e",
+                    "set -o pipefail",
+                    "git init .",
+                    "git config user.email test@rultor.com",
+                    "git config user.name test",
+                    "echo 'hello, world!' > hello.txt",
+                    "git add .",
+                    "git commit -am 'first file'",
+                    "git checkout -b frk",
+                    "echo 'good bye!' > hello.txt",
+                    "git commit -am 'modified file'",
+                    "git checkout master",
+                    "git config receive.denyCurrentBranch ignore"
+                )
+            ).directory(repo)
+        ).stdout();
+        return repo;
     }
 
 }
