25d51bb0210cc50e19fec491d08754f29ffbe747
Yegor Bugayenko <yegor@tpc2.com>
2014-09-07 15:01:13 +0200

    #578 git show-branch

--- src/main/resources/com/rultor/agents/req/release.sh
+++ src/main/resources/com/rultor/agents/req/release.sh
@@ -11,9 +11,9 @@ if [ $(git tag -l "${tag}") ]; then
   exit -1
 fi
 
-export BRANCH_NAME=
-while [ $(git branch --list "${BRANCH_NAME}" | wc -l) -gt 0 ]; do
-  export BRANCH_NAME="__rultor-tmp-$(cat /dev/urandom | tr -cd 'a-z0-9' | head -c 16)"
+export BRANCH_NAME=__rultor
+while [ $(git show-branch "${BRANCH_NAME}" | wc -l) -gt 0 ]; do
+  export BRANCH_NAME="__rultor-$(cat /dev/urandom | tr -cd 'a-z0-9' | head -c 16)"
 done
 git checkout -b "${BRANCH_NAME}"
 
