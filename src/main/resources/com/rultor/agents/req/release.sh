f916070ee88c2fad137cefbd297457627899287e
Yegor Bugayenko <yegor@tpc2.com>
2014-09-15 14:52:41 +0200

    hide stderr

--- src/main/resources/com/rultor/agents/req/release.sh
+++ src/main/resources/com/rultor/agents/req/release.sh
@@ -12,7 +12,7 @@ if [ $(git tag -l "${tag}") ]; then
 fi
 
 export BRANCH_NAME=__rultor
-while [ $(git show-branch "${BRANCH_NAME}" | wc -l) -gt 0 ]; do
+while [ $(git show-branch "${BRANCH_NAME}" 2>/dev/null | wc -l) -gt 0 ]; do
   export BRANCH_NAME="__rultor-$(cat /dev/urandom | tr -cd 'a-z0-9' | head -c 16)"
 done
 git checkout -b "${BRANCH_NAME}"
