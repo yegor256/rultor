08bf2759c4b1acc88701b0a80ce11f0413b4acd6
Yegor Bugayenko <yegor@tpc2.com>
2014-09-28 12:20:19 +0200

    more OOP design of Agents

--- src/main/java/com/rultor/agents/Agents.java
+++ src/main/java/com/rultor/agents/Agents.java
@@ -73,7 +73,6 @@ import com.rultor.spi.Talk;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.LinkedList;
 import java.util.concurrent.TimeUnit;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
@@ -144,103 +143,101 @@ public final class Agents {
      * @param profile Profile
      * @return List of them
      * @throws IOException If fails
+     * @checkstyle LineLength (150 lines)
      */
     public Collection<Agent> agents(final Talk talk, final Profile profile)
         throws IOException {
-        final Collection<Agent> agents = new LinkedList<Agent>();
         final Locks locks = this.sttc.locks();
-        final Question list = new Question.FirstOf(
-            Arrays.asList(
-                new QnIfContains("config", new QnConfig(profile)),
-                new QnIfContains("status", new QnStatus(talk)),
-                new QnIfContains("version", new QnVersion()),
-                new QnIfContains("hello", new QnHello()),
-                new QnIfCollaborator(
-                    new QnAlone(
-                        talk, locks,
-                        new Question.FirstOf(
-                            Arrays.<Question>asList(
-                                new QnIfContains(
-                                    "merge",
-                                    new QnAskedBy(
-                                        profile,
-                                        this.commanders("merge"),
-                                        new QnMerge()
-                                    )
-                                ),
-                                new QnIfContains(
-                                    "deploy",
-                                    new QnAskedBy(
-                                        profile,
-                                        this.commanders("deploy"),
-                                        new QnDeploy()
-                                    )
-                                ),
-                                new QnIfContains(
-                                    "release",
-                                    new QnAskedBy(
-                                        profile,
-                                        this.commanders("release"),
-                                        new QnRelease()
+        return Arrays.<Agent>asList(
+            new Understands(
+                this.github,
+                new QnSince(
+                    // @checkstyle MagicNumber (1 line)
+                    49092213,
+                    new QnReferredTo(
+                        this.github.users().self().login(),
+                        new QnParametrized(
+                            new Question.FirstOf(
+                                Arrays.asList(
+                                    new QnIfContains("config", new QnConfig(profile)),
+                                    new QnIfContains("status", new QnStatus(talk)),
+                                    new QnIfContains("version", new QnVersion()),
+                                    new QnIfContains("hello", new QnHello()),
+                                    new QnIfCollaborator(
+                                        new QnAlone(
+                                            talk, locks,
+                                            new Question.FirstOf(
+                                                Arrays.<Question>asList(
+                                                    new QnIfContains(
+                                                        "merge",
+                                                        new QnAskedBy(
+                                                            profile,
+                                                            this.commanders("merge"),
+                                                            new QnMerge()
+                                                        )
+                                                    ),
+                                                    new QnIfContains(
+                                                        "deploy",
+                                                        new QnAskedBy(
+                                                            profile,
+                                                            this.commanders("deploy"),
+                                                            new QnDeploy()
+                                                        )
+                                                    ),
+                                                    new QnIfContains(
+                                                        "release",
+                                                        new QnAskedBy(
+                                                            profile,
+                                                            this.commanders("release"),
+                                                            new QnRelease()
+                                                        )
+                                                    )
+                                                )
+                                            )
+                                        )
                                     )
                                 )
                             )
                         )
                     )
                 )
-            )
+            ),
+            new StartsRequest(profile),
+            new RegistersShell(
+                // @checkstyle MagicNumber (1 line)
+                "b1.rultor.com", 22,
+                "rultor",
+                IOUtils.toString(
+                    this.getClass().getResourceAsStream("rultor.key"),
+                    CharEncoding.UTF_8
+                )
+            ),
+            new StartsDaemon(profile),
+            new KillsDaemon(TimeUnit.HOURS.toMinutes(2L)),
+            new EndsDaemon(),
+            new EndsRequest(),
+            new Tweets(
+                this.github,
+                new OAuthTwitter(
+                    Manifests.read("Rultor-TwitterKey"),
+                    Manifests.read("Rultor-TwitterSecret"),
+                    Manifests.read("Rultor-TwitterToken"),
+                    Manifests.read("Rultor-TwitterTokenSecret")
+                )
+            ),
+            new CommentsTag(this.github),
+            new Reports(this.github),
+            new RemovesShell(),
+            new ArchivesDaemon(
+                new ReRegion(
+                    new Region.Simple(
+                        Manifests.read("Rultor-S3Key"),
+                        Manifests.read("Rultor-S3Secret")
+                    )
+                ).bucket(Manifests.read("Rultor-S3Bucket"))
+            ),
+            new Publishes(profile)
         );
-        agents.addAll(
-            Arrays.asList(
-                new Understands(
-                    this.github,
-                    new QnSince(
-                        // @checkstyle MagicNumber (1 line)
-                        49092213,
-                        new QnReferredTo(
-                            this.github.users().self().login(),
-                            new QnParametrized(list)
-                        )
-                    )
-                ),
-                new StartsRequest(profile),
-                new RegistersShell(
-                    // @checkstyle MagicNumber (1 line)
-                    "b1.rultor.com", 22,
-                    "rultor",
-                    IOUtils.toString(
-                        this.getClass().getResourceAsStream("rultor.key"),
-                        CharEncoding.UTF_8
-                    )
-                ),
-                new StartsDaemon(profile),
-                new KillsDaemon(TimeUnit.HOURS.toMinutes(2L)),
-                new EndsDaemon(),
-                new EndsRequest(),
-                new Tweets(
-                    this.github,
-                    new OAuthTwitter(
-                        Manifests.read("Rultor-TwitterKey"),
-                        Manifests.read("Rultor-TwitterSecret"),
-                        Manifests.read("Rultor-TwitterToken"),
-                        Manifests.read("Rultor-TwitterTokenSecret")
-                    )
-                ),
-                new CommentsTag(this.github),
-                new Reports(this.github),
-                new RemovesShell(),
-                new ArchivesDaemon(
-                    new ReRegion(
-                        new Region.Simple(
-                            Manifests.read("Rultor-S3Key"),
-                            Manifests.read("Rultor-S3Secret")
-                        )
-                    ).bucket(Manifests.read("Rultor-S3Bucket"))
-                ),
-                new Publishes(profile)
-            )
-        );
-        return agents;
     }
 
     /**
