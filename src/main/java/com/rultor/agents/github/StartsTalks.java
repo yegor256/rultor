5b70956dcdae777ee4a520ac3cc65b5390d49765
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2014-11-17 17:14:31 -0200

    spaces

--- src/main/java/com/rultor/agents/github/StartsTalks.java
+++ src/main/java/com/rultor/agents/github/StartsTalks.java
@@ -159,12 +159,12 @@ public final class StartsTalks implements SuperAgent {
         );
         talk.active(true);
         this.github.entry().uri()
-                .path("/users/follow").queryParam("target", talk.name())
-                .back().method(Request.POST)
-                .fetch();
+            .path("/users/follow").queryParam("target", talk.name())
+            .back().method(Request.POST)
+            .fetch();
         Logger.info(
-                this, "talk %s#%d activated as %s",
-                coords, issue.number(), name
+            this, "talk %s#%d activated as %s",
+            coords, issue.number(), name
         );
         return talk.name();
     }
