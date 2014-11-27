2b34845d045de69e7abeb75dbd277f9fe6f33240
Carlos Alexandro Becker <caarlos0@gmail.com>
2014-11-26 23:24:20 -0200

    #543 Using new jcabi-xml XSLDocument.STRIP

--- src/main/java/com/rultor/dynamo/DyTalk.java
+++ src/main/java/com/rultor/dynamo/DyTalk.java
@@ -37,7 +37,6 @@ import com.jcabi.dynamo.Item;
 import com.jcabi.xml.StrictXML;
 import com.jcabi.xml.XML;
 import com.jcabi.xml.XMLDocument;
-import com.jcabi.xml.XSL;
 import com.jcabi.xml.XSLDocument;
 import com.rultor.spi.Talk;
 import java.io.IOException;
@@ -60,14 +59,6 @@ import org.xembly.Xembler;
 @ToString
 @EqualsAndHashCode(of = "item")
 public final class DyTalk implements Talk {
-
-    /**
-     * Upgrade XSL.
-     */
-    private static final XSL CLEANUP = XSLDocument.make(
-        DyTalk.class.getResourceAsStream("cleanup.xsl")
-    );
-
     /**
      * Item.
      */
@@ -124,9 +115,9 @@ public final class DyTalk implements Talk {
                     ex
                 );
             }
-            final String body = DyTalk.CLEANUP.transform(
+            final String body = XSLDocument.STRIP.transform(
                 new StrictXML(new XMLDocument(node), Talk.SCHEMA)
-            ).toString().replaceAll("\\/s+", "");
+            ).toString();
             if (body.length() > Tv.FIFTY * Tv.THOUSAND) {
                 throw new IllegalArgumentException("XML is too big");
             }
