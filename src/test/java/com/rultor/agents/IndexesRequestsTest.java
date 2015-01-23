9544a264725e4576aee195db73ff764cc4d305b5
Dmitri Pisarenko <dp@altruix.co>
2015-01-23 15:22:40 +0300

    Removed diagnostic messages.

--- src/test/java/com/rultor/agents/IndexesRequestsTest.java
+++ src/test/java/com/rultor/agents/IndexesRequestsTest.java
@@ -30,7 +30,6 @@
 package com.rultor.agents;
 
 import com.jcabi.matchers.XhtmlMatchers;
-import com.rultor.spi.Talk;
 import com.rultor.spi.Talks;
 import org.hamcrest.MatcherAssert;
 import org.hamcrest.Matchers;
@@ -132,24 +131,6 @@ public final class IndexesRequestsTest {
                 .add("args").up()
                 .add("type").set("merge").up()
         );
-
-        final Iterable<Talk> activeTalks = talks.active();
-
-        System.out.println("activeTalks (START)");
-
-        for (final Talk talk : activeTalks) {
-            System.out.println("Talk XML: " + talk.read().toString());
-        }
-
-        System.out.println("activeTalks (END)");
-
-        System.out.println("Index first: " +
-            new IndexesRequests().index(talks.get(first)));
-        System.out.println("Index second: " +
-            new IndexesRequests().index(talks.get(second)));
-        System.out.println("Index third: " +
-            new IndexesRequests().index(talks.get(third)));
-
         new IndexesRequests().execute(talks);
         MatcherAssert.assertThat(
             talks.get(third).read(),
