b74d26e951081b7328a8f4c0a10fa3baa89e0fd5
Yegor Bugayenko <yegor@tpc2.com>
2015-04-28 17:42:57 -0700

    log

--- src/main/java/com/rultor/agents/daemons/WipesDaemon.java
+++ src/main/java/com/rultor/agents/daemons/WipesDaemon.java
@@ -30,6 +30,7 @@
 package com.rultor.agents.daemons;
 
 import com.jcabi.aspects.Immutable;
+import com.jcabi.log.Logger;
 import com.jcabi.xml.XML;
 import com.rultor.agents.AbstractAgent;
 import lombok.EqualsAndHashCode;
@@ -58,6 +59,7 @@ public final class WipesDaemon extends AbstractAgent {
 
     @Override
     public Iterable<Directive> process(final XML xml) {
+        Logger.warn(this, "daemon wiped: %s", xml.xpath("/talk/@name").get(0));
         return new Directives().xpath("/talk/daemon").remove();
     }
 
