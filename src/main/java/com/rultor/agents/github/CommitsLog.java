2dfd0953dbdf380814df5cc699d2cde838e32417
Yegor Bugayenko <yegor@tpc2.com>
2015-05-26 09:57:12 -0700

    #857 fix in CommitsLog

--- src/main/java/com/rultor/agents/github/CommitsLog.java
+++ src/main/java/com/rultor/agents/github/CommitsLog.java
@@ -93,29 +93,46 @@ final class CommitsLog {
             this.repo.commits().iterate(params)
         );
         int count = 0;
-        final StringBuilder line = new StringBuilder(0);
         for (final RepoCommit.Smart commit : commits) {
             if (count > Tv.TWENTY) {
                 lines.add(" * and more...");
                 break;
             }
-            final JsonObject json = commit.json();
-            line.setLength(0);
-            line.append(" * ").append(commit.sha());
-            line.append(" by @").append(
-                json.getJsonObject("author").getString("login")
-            );
-            if (!json.getJsonObject("commit").isNull("message")) {
-                line.append(": ").append(
-                    commit.message()
-                        .replaceAll("[\\p{Cntrl}\\p{Space}]+", " ")
-                        .replaceAll("(?<=^.{30}).+$", "...")
-                );
-            }
-            lines.add(line.toString());
+            lines.add(CommitsLog.asText(commit));
             ++count;
         }
         return Joiner.on('\n').join(lines);
+    }
+
+    /**
+     * Convert commit to text.
+     * @param commit The commit
+     * @return Text
+     * @throws IOException If fails
+     * @link https://developer.github.com/v3/repos/commits/#list-commits-on-a-repository
+     * @checkstyle MultipleStringLiteralsCheck (50 lines)
+     */
+    private static String asText(final RepoCommit.Smart commit)
+        throws IOException {
+        final StringBuilder line = new StringBuilder(Tv.HUNDRED);
+        final JsonObject json = commit.json();
+        line.append(" * ").append(commit.sha());
+        if (!json.isNull("author")) {
+            final JsonObject author = json.getJsonObject("author");
+            if (!author.isNull("login")) {
+                line.append(" by @").append(
+                    author.getString("login")
+                );
+            }
+        }
+        if (!json.getJsonObject("commit").isNull("message")) {
+            line.append(": ").append(
+                commit.message()
+                    .replaceAll("[\\p{Cntrl}\\p{Space}]+", " ")
+                    .replaceAll("(?<=^.{30}).+$", "...")
+            );
+        }
+        return line.toString();
     }
 
 }
