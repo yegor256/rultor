b31fa026c0b2b9534c6f0dc4397db7d56114c70f
Yegor Bugayenko <yegor@tpc2.com>
2015-10-07 16:25:28 -0700

    #942 delete images with -f flag

--- src/main/resources/com/rultor/agents/docker/rmi.sh
+++ src/main/resources/com/rultor/agents/docker/rmi.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-images=$(docker images | grep -E "[0-9]+ months ago" | tr -s ' ' | cut -d ' ' -f 3)
+images=$(docker images | grep -E '([0-9]{2,} weeks|[0-9]+ months) ago' | tr -s ' ' | cut -d ' ' -f 3 | uniq)
 for i in ${images}; do
-  docker rmi "${i}" || echo "failed to remove image ${i}"
+  docker rmi -f "${i}"
 done
