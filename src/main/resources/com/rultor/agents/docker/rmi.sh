31797b918a88a5bcddf49b4ac28e8758b32647da
Yegor Bugayenko <yegor@tpc2.com>
2015-10-07 21:03:14 -0700

    #942 no failure

--- src/main/resources/com/rultor/agents/docker/rmi.sh
+++ src/main/resources/com/rultor/agents/docker/rmi.sh
@@ -3,5 +3,5 @@ set -e
 
 images=$(docker images | grep -E '([0-9]{2,} weeks|[0-9]+ months) ago' | tr -s ' ' | cut -d ' ' -f 3 | uniq)
 for i in ${images}; do
-  docker rmi -f "${i}"
+  docker rmi -f "${i}" || echo "failed to remove image ${i}"
 done
