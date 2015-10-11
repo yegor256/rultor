7c874cb7cd06360fcab58c8c23ecff4e27657676
Yegor Bugayenko <yegor@tpc2.com>
2015-10-10 19:27:07 -0700

    #942 disable RMI

--- src/main/resources/com/rultor/agents/docker/rmi.sh
+++ src/main/resources/com/rultor/agents/docker/rmi.sh
@@ -3,5 +3,6 @@ set -e
 
 images=$(docker images | grep -E '([0-9]{2,} weeks|[0-9]+ months) ago' | tr -s ' ' | cut -d ' ' -f 3 | uniq)
 for i in ${images}; do
-  docker rmi -f "${i}" || echo "failed to remove image ${i}"
+  echo "nothing for now"
+  # echo docker rmi -f "${i}" || echo "failed to remove image ${i}"
 done
