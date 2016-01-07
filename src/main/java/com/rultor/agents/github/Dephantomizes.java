88e1860c82ee0cd3cca5a1aee71692f37f794c9f
Yegor Bugayenko <yegor@tpc2.com>
2016-01-07 10:22:29 +0200

    #973 Dephantomizes

---
+++ src/main/java/com/rultor/agents/github/Dephantomizes.java
@@ -0,0 +1,87 @@
+/**
+ * Copyright (c) 2009-2015, rultor.com
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met: 1) Redistributions of source code must retain the above
+ * copyright notice, this list of conditions and the following
+ * disclaimer. 2) Redistributions in binary form must reproduce the above
+ * copyright notice, this list of conditions and the following
+ * disclaimer in the documentation and/or other materials provided
+ * with the distribution. 3) Neither the name of the rultor.com nor
+ * the names of its contributors may be used to endorse or promote
+ * products derived from this software without specific prior written
+ * permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT
+ * NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
+ * THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.rultor.agents.github;
+
+import com.jcabi.aspects.Immutable;
+import com.jcabi.github.Github;
+import com.jcabi.github.Issue;
+import com.jcabi.log.Logger;
+import com.jcabi.xml.XML;
+import com.rultor.agents.AbstractAgent;
+import java.io.IOException;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+import org.xembly.Directive;
+import org.xembly.Directives;
+
+/**
+ * Removes request, if Github issue is gone.
+ *
+ * @author Yegor Bugayenko (yegor@teamed.io)
+ * @version $Id$
+ * @since 1.59.7
+ */
+@Immutable
+@ToString
+@EqualsAndHashCode(callSuper = false, of = "github")
+public final class Dephantomizes extends AbstractAgent {
+
+    /**
+     * Github.
+     */
+    private final transient Github github;
+
+    /**
+     * Ctor.
+     * @param ghub Github client
+     */
+    public Dephantomizes(final Github ghub) {
+        super(
+            "/talk/wire[github-repo and github-issue]",
+            "/talk/request[@id]"
+        );
+        this.github = ghub;
+    }
+
+    @Override
+    public Iterable<Directive> process(final XML xml) throws IOException {
+        final Directives dirs = new Directives();
+        final Issue.Smart issue = new TalkIssues(this.github, xml).get();
+        if (!issue.exists()) {
+            dirs.xpath("/talk/request").remove();
+            Logger.warn(
+                this, "issue #%d in %s is gone, deleting request",
+                issue.number(), issue.repo().coordinates()
+            );
+        }
+        return dirs;
+    }
+
+}
