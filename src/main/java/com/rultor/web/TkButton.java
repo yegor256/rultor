b361cf214cb90b1cf2381919b45a75c15b13ecf5
Yegor Bugayenko <yegor@tpc2.com>
2016-01-25 20:53:52 -0800

    #988 two sizes for button

--- src/main/java/com/rultor/web/TkButton.java
+++ src/main/java/com/rultor/web/TkButton.java
@@ -29,9 +29,13 @@
  */
 package com.rultor.web;
 
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.Locale;
 import org.takes.Response;
 import org.takes.facets.fork.RqRegex;
 import org.takes.facets.fork.TkRegex;
+import org.takes.rq.RqHref;
 import org.takes.rs.RsWithBody;
 import org.takes.rs.RsWithHeaders;
 import org.takes.rs.RsWithType;
@@ -46,11 +50,19 @@ import org.takes.rs.RsWithType;
 final class TkButton implements TkRegex {
 
     @Override
-    public Response act(final RqRegex req) {
+    public Response act(final RqRegex req) throws IOException {
+        final Iterator<String> size = new RqHref.Base(req)
+            .href().param("size").iterator();
+        String suffix = "m";
+        if (size.hasNext()) {
+            suffix = size.next().toLowerCase(Locale.ENGLISH);
+        }
         return new RsWithType(
             new RsWithHeaders(
                 new RsWithBody(
-                    this.getClass().getResourceAsStream("button.svg")
+                    this.getClass().getResourceAsStream(
+                        String.format("button-%s.svg", suffix)
+                    )
                 ),
                 "Cache-Control: no-cache"
             ),
