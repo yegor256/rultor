5a6303b7d9b10c777cab90a23ccbdad2d19d9ccc
Viktor Kuchyn <kuchin.victor@gmail.com>
2016-02-28 10:07:06 +0530

    #981: fixed Header check. Excluded target folder. Added years to rultor

--- years.sh
+++ years.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if (grep -r "Copyright[[:space:]]\+(c)[[:space:]]\+2009-.*rultor.com" --exclude-dir ".git" --exclude "years.sh" . | grep -v 2009-`date +%Y`) then
+if (grep -r "Copyright[[:space:]]\+(c)[[:space:]]\+2009-.*rultor.com" --exclude-dir "target" --exclude-dir ".git" --exclude "years.sh" . | grep -v 2009-`date +%Y`) then
     echo "Files above have wrong years in copyrights"
     exit 1
 fi
