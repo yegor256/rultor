65fbea73c436c7669c75fbc6e51a1dcbafa433ee
Viktor Kuchyn <kuchin.victor@gmail.com>
2016-02-29 20:28:21 +0530

    #981: refactored whitespace

--- years.sh
+++ years.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-if (grep -r "Copyright[[:space:]]\+(c)[[:space:]]\+2009-.*rultor.com" \
+if (grep -r "Copyright \+(c) \+2009-.*rultor.com" \
   --exclude-dir "target" --exclude-dir ".git" --exclude "years.sh" . \
   | grep -v 2009-`date +%Y`) then
     echo "Files above have wrong years in copyrights"
     exit 1
 fi
 
-if (grep -r -L "Copyright[[:space:]]\+(c)[[:space:]]\+2009-.*rultor.com" \
+if (grep -r -L "Copyright \+(c) \+2009-.*rultor.com" \
  --include=*.{java,xml,vm,groovy,txt,fml,properties} --exclude-dir "target" . \
  | grep -v 2009-`date +%Y`) then
     echo "Files above must have copyright block in header"
