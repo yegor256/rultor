23141043623f4625acf3785b7161a586d0df4e80
Viktor Kuchyn <kuchin.victor@gmail.com>
2016-03-01 18:53:41 +0530

    #981: fixed include

--- years.sh
+++ years.sh
@@ -2,14 +2,15 @@
 
 if (grep -r "Copyright \+(c) \+2009-.*rultor.com" \
   --exclude-dir "target" --exclude-dir ".git" --exclude "years.sh" . \
-  | grep -v 2009-`date +%Y`) then
+  | grep -v 2009-`date +%Y`); then
     echo "Files above have wrong years in copyrights"
     exit 1
 fi
 
 if (grep -r -L "Copyright \+(c) \+2009-.*rultor.com" \
- --include=*.{java,xml,vm,groovy,txt,fml,properties} --exclude-dir "target" . \
- | grep -v 2009-`date +%Y`) then
+ --include=*.java --include=*.xml --include=*.vm --include=*.groovy \
+ --include=*.txt --include=*.fml --include=*.properties} \
+ --exclude-dir "target" .); then
     echo "Files above must have copyright block in header"
     exit 1
 fi
