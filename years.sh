5aa24eaf8c914f13a5172cca8b9ba3ab54f4c264
Viktor Kuchyn <kuchin.victor@gmail.com>
2016-03-02 20:36:14 +0530

    #981: excluded apache-maven folder for ci

--- years.sh
+++ years.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 if (grep -r "Copyright \+(c) \+2009-.*rultor.com" \
-  --exclude-dir "target" --exclude-dir ".git" --exclude "years.sh" . \
-  | grep -v 2009-`date +%Y`); then
+  --exclude-dir "target" --exclude-dir ".git" --exclude "years.sh" \
+  --exclude-dir "apache-maven-*" . | grep -v 2009-`date +%Y`); then
     echo "Files above have wrong years in copyrights"
     exit 1
 fi
@@ -10,7 +10,8 @@ fi
 if (grep -r -L "Copyright \+(c) \+2009-.*rultor.com" \
   --include=*.java --include=*.xml --include=*.vm --include=*.groovy \
   --include=*.txt --include=*.fml --include=*.properties} \
-  --exclude-dir "target" . | grep -v 2009-`date +%Y`); then
+  --exclude-dir "target" --exclude-dir "apache-maven-*" . \
+  | grep -v 2009-`date +%Y`); then
     echo "Files above must have copyright block in header"
     exit 1
 fi
