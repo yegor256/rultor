e506072b7ec441b5ed91b34380bae2e378461776
Armin Braun <armin.b@icanlocalize.com>
2016-03-04 12:55:48 +0100

    exclude settings.xml from copyright check to fix release

--- years.sh
+++ years.sh
@@ -2,7 +2,7 @@
 
 if (grep -r "Copyright \+(c) \+2009-.*rultor.com" \
   --exclude-dir "target" --exclude-dir ".git" --exclude "years.sh" \
-  --exclude-dir ".idea" \
+  --exclude-dir ".idea" --exclude "settings.xml" \
   --exclude-dir "apache-maven-*" . | grep -v 2009-`date +%Y`); then
     echo "Files above have wrong years in copyrights"
     exit 1
@@ -12,7 +12,7 @@ if (grep -r -L "Copyright \+(c) \+2009-.*rultor.com" \
   --include=*.java --include=*.xml --include=*.vm --include=*.groovy \
   --include=*.txt --include=*.fml --include=*.properties} \
   --exclude-dir "target" --exclude-dir "apache-maven-*" . \
-  --exclude-dir ".idea" \
+  --exclude-dir ".idea" --exclude "settings.xml" \
   | grep -v 2009-`date +%Y`); then
     echo "Files above must have copyright block in header"
     exit 1
