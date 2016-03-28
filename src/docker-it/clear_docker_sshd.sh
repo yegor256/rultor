71727ead83feea07a51641d9b6098dac44afb864
Viktor Kuchyn <kuchin.victor@gmail.com>
2016-03-28 19:25:21 +0300

    #1055: fixes

--- src/docker-it/clear_docker_sshd.sh
+++ src/docker-it/clear_docker_sshd.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-container=rultor_sshd
+container="rultor_sshd"
 image="${container}_img"
-echo removing docker container $container
-docker stop $container
-docker rm $container
+echo "Removing docker container $container"
+docker stop "$container"
+docker rm "$container"
