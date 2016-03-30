5627d65cf581bcffeba42b2e266b84d78ca5c8ff
Viktor Kuchyn <kuchin.victor@gmail.com>
2016-03-30 13:27:44 +0300

    #1055: fixed cid, removed ip

--- src/docker-it/clear_docker_sshd.sh
+++ src/docker-it/clear_docker_sshd.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-container="rultor_sshd"
+# requires command line argument with path to rultor root folder
+if ($1); then
+    echo "Required path to rultor root folder"
+    exit 1
+fi
+echo "Path to rultor root folder is $1"
 image="${container}_img"
-echo "Removing docker container $container"
-docker stop "$container"
-docker rm "$container"
+echo "Removing docker container $(<$1/target/cid)"
+docker rm -f "$(<$1/target/cid)"
+rm $1/target/cid
