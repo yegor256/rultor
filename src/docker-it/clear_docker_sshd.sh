c57498dea171fd7b75dbc6fbf0087a8f3c08fbb8
Armin Braun <armin.b@icanlocalize.com>
2016-03-30 20:34:22 +0200

    SSHd docker container polish

--- src/docker-it/clear_docker_sshd.sh
+++ src/docker-it/clear_docker_sshd.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-# requires command line argument with path to rultor root folder
-if ($1); then
-    echo "Required path to rultor root folder"
+# requires command line argument with path to Rultor root folder
+if [ -z "$1" ]; then
+    echo "Required path to Rultor root folder"
     exit 1
 fi
-echo "Path to rultor root folder is $1"
-image="${container}_img"
-echo "Removing docker container $(<$1/target/cid)"
-docker rm -f "$(<$1/target/cid)"
-rm $1/target/cid
+if [ -z "$2" ]; then
+    echo "Required Docker host not given!"
+    exit 1
+fi
+echo "Removing Docker container $(<$1/target/docker-it/cid)"
+docker -H $2 rm -f "$(<$1/target/docker-it/cid)"
+rm -f $1/target/docker-it/cid
--- src/docker-it/run_docker_sshd.sh
+++ src/docker-it/run_docker_sshd.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
-# requires command line argument with path to rultor root folder
-if ($1); then
-    echo "Required path to rultor root folder"
+# requires command line argument with path to Rultor root folder
+if [ -z "$1" ]; then
+    echo "Required path to Rultor root folder not given!"
     exit 1
 fi
-echo "Path to rultor root folder is $1"
-dockerit=$1/src/docker-it
-image="rultor_sshd_img"
-echo "Image=$image"
-echo "Grant 600 permissions for keys"
-chmod 600 "$dockerit/id_rsa"
-chmod 600 "$dockerit/id_rsa.pub"
-echo "Building docker image $image"
-docker build -t $image "$dockerit"
-sh $dockerit/clear_docker_sshd.sh "$1"
-echo "Running docker container"
-docker run -d --cidfile="$1/target/cid" -p 22 "$image"
-echo "Exporting container port"
-rm "$1/target/sshd.port"
-docker inspect --format='{{(index (index .NetworkSettings.Ports "22/tcp") 0).HostPort}}' \
-    "$(<$1/target/cid)" >> "$1/target/sshd.port"
-echo "$(<$1/target/sshd.port)"
+if [ -z "$2" ]; then
+    echo "Required Docker host not given!"
+    exit 1
+fi
+echo "Path to rultor root folder is ${1}"
+dockerit="${1}/src/docker-it"
+image="rultor-sshd-it"
+chmod 600 "${dockerit}/id_rsa"
+chmod 600 "${dockerit}/id_rsa.pub"
+docker -H $2 build -t "${image}" "${dockerit}"
+mkdir -p ${1}/target/docker-it
+bash -l ${dockerit}/clear_docker_sshd.sh ${1} 2>/dev/null
+docker -H $2 run -d --name="$(dd if=/dev/urandom bs=10k count=1 2>/dev/null | tr -cd 'a-z0-9' | head -c 8)" \
+ --cidfile="${1}/target/docker-it/cid"  -p 22 "${image}"
+rm -f "${1}/target/docker-it/sshd.port"
+docker -H $2 inspect --format='{{(index (index .NetworkSettings.Ports "22/tcp") 0).HostPort}}' \
+    "$(<${1}/target/docker-it/cid)" >> "${1}/target/docker-it/sshd.port"
+echo "$(<${1}/target/docker-it/sshd.port)"
