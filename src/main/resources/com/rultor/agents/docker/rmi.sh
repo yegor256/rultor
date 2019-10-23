419aa4a56f5df6eca16f160f1c91957b088a075b
Yegor Bugayenko <yegor256@gmail.com>
2019-10-23 14:48:09 +0800

    #1315 typo

--- src/main/resources/com/rultor/agents/docker/rmi.sh
+++ src/main/resources/com/rultor/agents/docker/rmi.sh
@@ -5,8 +5,6 @@ images=$(docker images --no-trunc | grep -E '([0-9]{2,} weeks|[0-9]+ months) ago
 for i in ${images}; do
   containers=$(docker ps --filter="ancestor=${i}" | wc -l)
   if [ "${containers}" -eq "1" ]; then
-    echo "Docker image ${i} is being used, won't delete"
-  else
     docker rmi -f "${i}" || echo "Failed to remove Docker image ${i}"
   fi
 done
