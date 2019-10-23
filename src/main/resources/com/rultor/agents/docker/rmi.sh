0efee5ecf5cfacae6497c27d4aad40801c4c4100
Yegor Bugayenko <yegor256@gmail.com>
2019-10-23 13:52:05 +0800

    #1315 delete docker images

--- src/main/resources/com/rultor/agents/docker/rmi.sh
+++ src/main/resources/com/rultor/agents/docker/rmi.sh
@@ -3,6 +3,5 @@ set -e
 
 images=$(docker images | grep -E '([0-9]{2,} weeks|[0-9]+ months) ago' | tr -s ' ' | cut -d ' ' -f 3 | uniq)
 for i in ${images}; do
-  echo "no Docker images to delete for now"
-  # echo docker rmi -f "${i}" || echo "failed to remove image ${i}"
+  docker rmi -f "${i}" || echo "Failed to remove Docker image ${i}"
 done
