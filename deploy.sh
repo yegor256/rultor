4ecc65475fd8f332f4230113b0cb46ddfcfb0120
yegor256 <yegor256@gmail.com>
2020-04-22 07:59:10 +0300

    #1335 heroku

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ set -e
 cd $(dirname $0)
 cp /code/home/assets/rultor/settings.xml .
 git add settings.xml
-git commit -m 'settings.xml for dokku'
+git commit -m 'settings.xml for heroku'
 trap 'git reset HEAD~1 && rm settings.xml' EXIT
-git push dokku master -f
+git push heroku master -f
 
