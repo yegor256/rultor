ceb4c04bc6162a7e4f94d286612122872ccee473
Yegor Bugayenko <yegor256@gmail.com>
2022-07-20 09:29:17 +0300

    #1379 extra test

---
+++ src/test/java/com/rultor/agents/daemons/EndsDaemonTest.java
@@ -0,0 +1,80 @@
+/**
+ * Copyright (c) 2009-2022 Yegor Bugayenko
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met: 1) Redistributions of source code must retain the above
+ * copyright notice, this list of conditions and the following
+ * disclaimer. 2) Redistributions in binary form must reproduce the above
+ * copyright notice, this list of conditions and the following
+ * disclaimer in the documentation and/or other materials provided
+ * with the distribution. 3) Neither the name of the rultor.com nor
+ * the names of its contributors may be used to endorse or promote
+ * products derived from this software without specific prior written
+ * permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT
+ * NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
+ * THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.rultor.agents.daemons;
+
+import com.rultor.Time;
+import com.rultor.spi.Agent;
+import com.rultor.spi.Talk;
+import java.io.IOException;
+import java.net.UnknownHostException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.xembly.Directives;
+
+/**
+ * Tests for {@link EndsDaemon}.
+ *
+ * @author Yegor Bugayenko (yegor256@gmail.com)
+ * @version $Id$
+ * @checkstyle ClassDataAbstractionCouplingCheck (500 lines)
+ * @checkstyle MultipleStringLiterals (500 lines)
+ */
+public final class EndsDaemonTest {
+
+    /**
+     * EndsDaemon should fail if host is not found.
+     * @throws IOException In case of error.
+     */
+    @Test
+    public void failsIfHostNotFound() throws IOException {
+        final Talk talk = new Talk.InFile();
+        talk.modify(
+            new Directives().xpath("/talk")
+                .add("daemon")
+                .attr("id", "abcd")
+                .add("title").set("merge").up()
+                .add("script").set("ls").up()
+                .add("started").set(new Time().iso()).up()
+                .add("dir").set("/tmp").up()
+                .up()
+                .add("shell").attr("id", "a1b2c3e3")
+                .add("host").set("bad-host-name").up()
+                .add("port").set("2222").up()
+                .add("login").set("test").up()
+                .add("key").set("test")
+        );
+        final Agent agent = new EndsDaemon();
+        Assertions.assertThrows(
+            UnknownHostException.class,
+            () -> agent.execute(talk)
+        );
+    }
+
+}
