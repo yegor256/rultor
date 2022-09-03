be4104678622b20ef0bc0b84daf562dc57313d90
Yegor Bugayenko <yegor256@gmail.com>
2022-09-03 09:15:45 +0300

    #1388 rmi in background

--- src/main/resources/com/rultor/agents/docker/rmi.sh
+++ src/main/resources/com/rultor/agents/docker/rmi.sh
@@ -11,6 +11,6 @@ images=$(docker images --no-trunc | grep -E '([0-9]{2,} weeks|[0-9]+ months) ago
 for i in ${images}; do
   containers=$(docker ps --filter="ancestor=${i}" | wc -l)
   if [ "${containers}" -eq "1" ]; then
-    docker rmi -f "${i}" || echo "Failed to remove Docker image ${i}"
+    nohup docker rmi -f "${i}" >/dev/null 2>&1 &
   fi
 done
