2efdd98679aa8bebf8d3cca840d2ed75522fbd9f
Natalia Pozhidaeva <Natalia_Pozhidaeva@epam.com>
2024-02-14 22:56:29 -0500

    [#1701] Get pre value from the comment (if presented)

--- src/main/java/com/rultor/agents/github/CommentsTag.java
+++ src/main/java/com/rultor/agents/github/CommentsTag.java
@@ -135,7 +135,7 @@ public final class CommentsTag extends AbstractAgent {
                 rels.create(tag.trim())
             );
             rel.name(CommentsTag.title(req, issue));
-            rel.prerelease(this.isPrerelease());
+            rel.prerelease(this.isPrerelease(req));
             rel.body(
                 String.format(
                     // @checkstyle LineLength (1 line)
@@ -153,17 +153,24 @@ public final class CommentsTag extends AbstractAgent {
     /**
      * Check if release is prerelease.
      * True if profile does not specify release.pre=false.
+     * @param req Comment's xml
      * @return True if prerelease, false otherwise.
      */
-    private boolean isPrerelease() {
+    private boolean isPrerelease(final XML req) {
         try {
             final boolean result;
-            final List<String> xpath = this.profile.read()
-                .xpath("/p/entry[@key='release']/entry[@key='pre']/text()");
-            if (xpath.isEmpty()) {
-                result = true;
+            final List<String> comment = req
+                .xpath("args/arg[@name='pre']/text()");
+            if (comment.isEmpty()) {
+                final List<String> xpath = this.profile.read()
+                    .xpath("/p/entry[@key='release']/entry[@key='pre']/text()");
+                if (xpath.isEmpty()) {
+                    result = true;
+                } else {
+                    result = "true".equals(xpath.get(0).trim());
+                }
             } else {
-                result = "true".equals(xpath.get(0).trim());
+                result = "true".equals(comment.get(0).trim());
             }
             return result;
         } catch (final IOException exception) {
